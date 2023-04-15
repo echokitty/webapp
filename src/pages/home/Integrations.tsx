bfc52b594fae46bb54d47a7b1aff274e26195931
chase-manning <chase@manning.dev>
2023-04-15 21:11:06 +0900

    :fire: remove unsupported integrations

--- src/pages/home/Integrations.tsx
+++ src/pages/home/Integrations.tsx
@@ -18,14 +18,6 @@ const integrations: IntegrationType[] = [
     icon: "https://storage.googleapis.com/ethglobal-api-production/organizations%2F9z8z1%2Flogo%2F1664911999338_gnosis.jpeg",
   },
   {
-    name: "Taiko",
-    icon: "https://storage.googleapis.com/ethglobal-api-production/organizations%2Fqfmej%2Flogo%2F1676412617433_ycMVxfJt_400x400.jpeg",
-  },
-  {
-    name: "Mina Protocol",
-    icon: "https://storage.googleapis.com/ethglobal-api-production/organizations%2F6wi0d%2Flogo%2F1664912919187_mina.png",
-  },
-  {
     name: "Polygon",
     icon: "https://storage.googleapis.com/ethglobal-api-production/organizations%2F536ub%2Fimages%2FWhite%20on%20Gradient%20Square.png",
   },
@@ -34,20 +26,8 @@ const integrations: IntegrationType[] = [
     icon: "https://storage.googleapis.com/ethglobal-api-production/organizations%2Fvxwti%2Flogo%2F1678645512720_Screen%20Shot%202023-03-12%20at%2011.25.01%20AM.png",
   },
   {
-    name: "MetaMask",
-    icon: "https://storage.googleapis.com/ethglobal-api-production/organizations%2F4pn9u%2Flogo%2F1679944522371_2Ubgzfgg_400x400.jpeg",
-  },
-  {
-    name: "Celo",
-    icon: "https://storage.googleapis.com/ethglobal-api-production/organizations%2Finotq%2Flogo%2F1678316332198_InjXBNx9_400x400.jpeg",
-  },
-  {
     name: "Uniswap Foundation",
     icon: "https://storage.googleapis.com/ethglobal-api-production/organizations%2F026zc%2Fimages%2Fimageedit_19_7248710069.png",
-  },
-  {
-    name: "Scroll",
-    icon: "https://storage.googleapis.com/ethglobal-api-production/organizations%2Fyip67%2Fimages%2Fscroll---logo-logo-mark-full-color-rgb-266px%4072ppi.png",
   },
 ];
 
