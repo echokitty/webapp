513916e3a0d70731eb1ab3fb4517ae545823247b
chase-manning <chase@manning.dev>
2023-04-15 16:24:13 +0900

    :rocket: add overview to dashboard

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,17 +1,21 @@
 import styled from "styled-components";
+import Overview from "./Overview";
 
 const StyledDashboardPage = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
   flex: 1;
-  font-size: 2.3rem;
-  padding: 3rem;
-  border: solid 1px pink;
+  padding: var(--section-spacing);
 `;
 
 const DashboardPage = () => {
-  return <StyledDashboardPage>Dashboard page</StyledDashboardPage>;
+  return (
+    <StyledDashboardPage>
+      <Overview />
+    </StyledDashboardPage>
+  );
 };
 
 export default DashboardPage;
