7e7c63bc90891664656f64b380496938161b1fdc
chase-manning <chase@manning.dev>
2023-04-15 17:48:58 +0900

    :sparkles: setup translation support

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import { store } from "./state/store";
 import GlobalStyles from "./styles/GlobalStyles";
 import Routing from "./Routing";
+import "./app/i18n";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <GlobalStyles />
-      <Routing />
+      <Suspense fallback={<div />}>
+        <Routing />
+      </Suspense>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
