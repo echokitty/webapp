a3f296cf09f45587d064143e847c3bd31e4960a8
chase-manning <chase@manning.dev>
2023-04-15 16:24:02 +0900

    :sparkles: create overview component

---
+++ src/pages/dashboard/Overview.tsx
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+
+import { Header } from "../../styles/Headers";
+import useOverview from "../../app/hooks/use-overview";
+import { roundToDp } from "../../app/lib/formatting";
+import PieChart from "../../components/PieChart";
+
+const StyledOverview = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 140rem;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+const Stats = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+const Chart = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 4rem;
+`;
+
+const ItemHeader = styled.div`
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-right: 1rem;
+`;
+
+const ItemValue = styled.div`
+  font-size: 4rem;
+  font-weight: 600;
+`;
+
+const Overview = () => {
+  const overview = useOverview();
+
+  const labels = overview.tokens.map((token) => token.tokenAddress);
+
+  const data = overview.tokens.map((token) => token.balance);
+
+  return (
+    <StyledOverview>
+      <Header>Dashboard</Header>
+      <Content>
+        <Stats>
+          <Item>
+            <ItemHeader>Total Balance:</ItemHeader>
+            <ItemValue>{`$${overview.balance.toLocaleString()}`}</ItemValue>
+          </Item>
+          <Item>
+            <ItemHeader>PNL:</ItemHeader>
+            <ItemValue>{`${roundToDp(
+              overview.pnl
+            ).toLocaleString()}%`}</ItemValue>
+          </Item>
+        </Stats>
+        <Chart>
+          <PieChart labels={labels} data={data} />
+        </Chart>
+      </Content>
+    </StyledOverview>
+  );
+};
+
+export default Overview;
