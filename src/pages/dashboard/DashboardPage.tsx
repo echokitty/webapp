0f8a8046ab1875bdb8e3d73e7f0b458427d2d349
chase-manning <chase@manning.dev>
2023-04-16 02:57:45 +0900

    :fire: remove redundant logging

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -5,7 +5,6 @@ import Overview from "./Overview";
 import Positions from "./Positions";
 import CreatePosition from "./CreatePosition";
 import WalletSelector from "../../components/WalletSelector";
-import { usePositions, useSubwallets, useWallet } from "../../contracts/views";
 
 const StyledDashboardPage = styled.div`
   width: 100%;
@@ -23,15 +22,6 @@ const StyledDashboardPage = styled.div`
 
 const DashboardPage = () => {
   const { address, action } = useParams<"address" | "action">();
-
-  const wallet = useWallet();
-  console.log(wallet);
-
-  const wallets = useSubwallets();
-  console.log(wallets);
-
-  const positions = usePositions();
-  console.log("positions", positions);
 
   return (
     <StyledDashboardPage>
