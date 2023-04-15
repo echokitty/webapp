b761e36571f65f27cea24e4dbe05dc35886bfb89
chase-manning <chase@manning.dev>
2023-04-16 05:11:03 +0900

    :rocket: add live positions

--- src/pages/dashboard/CreatePosition.tsx
+++ src/pages/dashboard/CreatePosition.tsx
@@ -2,10 +2,16 @@ import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { utils, BigNumber } from "ethers";
 
 import Popup from "../../components/Popup";
 import Input from "../../styles/Input";
 import Radio from "../../components/Radio";
+import useTokens, { Token } from "../../app/hooks/use-tokens";
+import { ZERO_ADDRESS } from "../../app/globals";
+import usePrices from "../../app/hooks/use-prices";
+import { roundToDp } from "../../app/lib/formatting";
+import { Swap, useCreatePosition } from "../../contracts/functions";
 
 const StyledCreatePosition = styled.div`
   display: flex;
@@ -62,8 +68,15 @@ interface Props {
 
 const CreatePosition = ({ show, address }: Props) => {
   const { t } = useTranslation();
+  const [selected, setSelected] = useState<number[]>([]);
   const [address_, setAddress] = useState<string | undefined>(undefined);
   const [amount, setAmount] = useState<string | undefined>(undefined);
+  const tokens = useTokens("matic-mainnet", address_ || ZERO_ADDRESS);
+  const prices = usePrices(tokens.map((token: Token) => token.address));
+  const { createPositionState, createPosition } = useCreatePosition(
+    address_ || ZERO_ADDRESS,
+    BigNumber.from(amount || "0").mul(BigNumber.from(10).pow(18))
+  );
 
   const navigate = useNavigate();
 
@@ -78,13 +91,47 @@ const CreatePosition = ({ show, address }: Props) => {
   const createAddress: string = t("create.address");
   const createAmount: string = t("create.amount");
 
+  let total = 0;
+  tokens.forEach((token: Token) => {
+    if (prices && prices[token.address]) {
+      total += token.balance * prices[token.address];
+    }
+  });
+
+  const getPercentage = (
+    token: Token,
+    prices: Record<string, number> | null
+  ) => {
+    if (!prices) return 0;
+    if (!prices[token.address]) return 0;
+    return (token.balance * prices[token.address]) / total;
+  };
+
+  const swaps: Swap[] = tokens
+    .filter((token: Token, index: number) => {
+      return selected.includes(index);
+    })
+    .map((token: Token) => {
+      const percentage = getPercentage(token, prices);
+      return {
+        toToken: token.address,
+        fromAmount: BigNumber.from(
+          Math.floor(Number(amount) * percentage * 10_000)
+        )
+          .mul(BigNumber.from(10).pow(14))
+          .toString(),
+      };
+    });
+
+  console.log(swaps);
+
   return (
     <Popup
       header={header}
       show={show}
       close={() => navigate("/dashboard")}
       buttonText={buttonText}
-      buttonAction={() => console.log("meow")}
+      buttonAction={() => createPosition(swaps)}
       width="60rem"
     >
       <StyledCreatePosition>
@@ -107,25 +154,28 @@ const CreatePosition = ({ show, address }: Props) => {
             <RowHeader>Percent</RowHeader>
             <RowHeader>Amount</RowHeader>
           </Row>
-          <Row>
-            <Radio checked toggle={() => console.log("meow")} />
-            <Item>USDC</Item>
-            <Item>50%</Item>
-            <Item>1000</Item>
-          </Row>
-
-          <Row>
-            <Radio checked toggle={() => console.log("meow")} />
-            <Item>USDC</Item>
-            <Item>50%</Item>
-            <Item>1000</Item>
-          </Row>
-          <Row>
-            <Radio checked toggle={() => console.log("meow")} />
-            <Item>USDC</Item>
-            <Item>50%</Item>
-            <Item>1000</Item>
-          </Row>
+          {tokens.map((token: Token, index: number) => {
+            return (
+              <Row>
+                <Radio
+                  checked={selected.includes(index)}
+                  toggle={() => {
+                    if (selected.includes(index)) {
+                      setSelected(selected.filter((i) => i !== index));
+                    } else {
+                      setSelected([...selected, index]);
+                    }
+                  }}
+                />
+                <Item>{token.symbol}</Item>
+                <Item>{`${roundToDp(
+                  getPercentage(token, prices) * 100,
+                  2
+                )}%`}</Item>
+                <Item>{`${token.balance} ${token.symbol}`}</Item>
+              </Row>
+            );
+          })}
         </ShoppingList>
       </StyledCreatePosition>
     </Popup>
