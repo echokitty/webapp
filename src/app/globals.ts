d99e490c2cf4cfc0c85aa551c35421c21fbcbec2
chase-manning <chase@manning.dev>
2023-04-16 02:01:23 +0900

    :wrench: add more globals

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,13 +1,15 @@
-export const LIVE = false;
+export const LIVE = true;
+
+export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 interface GlobalsType {
-  WETH: string;
+  FACTORY: string;
 }
 
 export const ETH_GLOBALS: GlobalsType = {
-  WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+  FACTORY: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
 };
 
-export const RINKEBY_GLOBALS: GlobalsType = {
-  WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+export const POLYGON_GLOBALS: GlobalsType = {
+  FACTORY: "0xBC80Dbb5E83Bf829E6487c30D72462098fda700C",
 };
