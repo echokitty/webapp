75aa1aec6c91f19154a19a6ed8982f6c89c3da0b
chase-manning <chase@manning.dev>
2023-04-15 20:37:51 +0900

    :lipstick: udpate how it works design

--- src/pages/home/HowItWorks.tsx
+++ src/pages/home/HowItWorks.tsx
@@ -25,13 +25,35 @@ const Items = styled.div`
 `;
 
 const Item = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
   margin: 0 1rem;
+  padding: 3rem;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: var(--blue);
+  opacity: 0.3;
+  border-radius: 2rem;
+`;
+
+const Number = styled.div`
+  position: absolute;
+  top: 2rem;
+  left: 2rem;
+  font-size: 2.4rem;
+  font-weight: 600;
 `;
 
 const ItemImage = styled.div`
+  position: relative;
   height: 8rem;
   width: 8rem;
   background-color: pink;
@@ -39,6 +61,7 @@ const ItemImage = styled.div`
 `;
 
 const ItemHeader = styled.h4`
+  position: relative;
   font-size: 3rem;
   font-weight: 600;
   margin-bottom: 0.7rem;
@@ -46,6 +69,7 @@ const ItemHeader = styled.h4`
 `;
 
 const ItemDescription = styled.p`
+  position: relative;
   font-size: 1.5rem;
   font-weight: 400;
   line-height: 1.2;
@@ -82,8 +106,10 @@ const HowItWorks = () => {
     <StyledHowItWorks>
       <Header>{t("howItWorks.title")}</Header>
       <Items>
-        {items.map((item: ItemType) => (
+        {items.map((item: ItemType, index: number) => (
           <Item key={item.label}>
+            <Background />
+            <Number>{`${index + 1}.`}</Number>
             <ItemImage />
             <ItemHeader>{item.label}</ItemHeader>
             <ItemDescription>{item.description}</ItemDescription>
